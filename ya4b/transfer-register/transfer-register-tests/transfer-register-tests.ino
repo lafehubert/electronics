f172ead63f7e7707927fdcf7fd9a443b456a71f3
Greg Martin <gmartin@bobshouse.net>
2020-03-10 19:57:57 -0700

    Rewrote transfer register tests using the test library

--- ya4b/transfer-register/transfer-register-tests/transfer-register-tests.ino
+++ ya4b/transfer-register/transfer-register-tests/transfer-register-tests.ino
@@ -1,41 +1,17 @@
-// This program will run tests on the YA4B transfer register
-// The transfer register allows the transfer of values to and from 
-// the 4-bit data registers to the 8-bit address registers.
+/*
+ * This program will run tests on the YA4B transfer register
+ * The transfer register allows the transfer of values to and from 
+ * the 4-bit data registers to the 8-bit address registers.
+ * 
+ */
 
-// Address Bus
-#define ADDR_0 22
-#define ADDR_1 24
-#define ADDR_2 26
-#define ADDR_3 28
-#define ADDR_4 30
-#define ADDR_5 32
-#define ADDR_6 34
-#define ADDR_7 36
-
-// Data Bus
-#define DATA_0 23
-#define DATA_1 25
-#define DATA_2 27
-#define DATA_3 29
-
-// Control Signals
-#define CTRL_LOAD_TH 40
-#define CTRL_LOAD_TL 42
-
-#define CTRL_OE_TH 44
-#define CTRL_OE_TL 46
-
-#define CTRL_OE_T 48
-
-#define CTRL_BUS_SELECT 50
-
-#define CTRL_CLK 52
+#include "Ya4bUtil.h"
 
 #define STR_HIGH "high"
 #define STR_LOW  "low"
 
-int address_pins[8] = { ADDR_0, ADDR_1, ADDR_2, ADDR_3, ADDR_4, ADDR_5, ADDR_6, ADDR_7 };
-int data_pins[4] = { DATA_0, DATA_1, DATA_2, DATA_3 };
+Ya4bUtil utils;
+
 const char* states[2] = { STR_LOW, STR_HIGH };
 
 void setup() {
@@ -43,46 +19,10 @@ void setup() {
   Serial.println("Starting up...");
   Serial.println("");
   randomSeed(analogRead(0));
-  setupPins();
+  utils.setupPins();
   runTests();
 }
 
-void setupPins() {
-  for (int i = CTRL_LOAD_TH; i <= CTRL_CLK; i+=2) {
-    if ((i != CTRL_CLK && i) != (CTRL_BUS_SELECT)) {
-      digitalWrite(i, HIGH);
-    } else {
-      digitalWrite(i, LOW);
-    }
-    pinMode(i, OUTPUT);
-  }
-  setDataInput();
-  setAddressInput();
-}
-
-void setDataInput() {
-  for (int i = DATA_0; i <= DATA_3; i+=2) {
-    pinMode(i, INPUT_PULLUP);
-  }
-}
-
-void setDataOutput() {
-  for (int i = DATA_0; i <= DATA_3; i+=2) {
-    pinMode(i, OUTPUT);
-  }
-}
-
-void setAddressInput() {
-  for (int i = ADDR_0; i <= ADDR_7; i+=2) {
-    pinMode(i, INPUT_PULLUP);
-  }
-}
-
-void setAddressOutput() {
-  for (int i = ADDR_0; i <= ADDR_7; i+=2) {
-    pinMode(i, OUTPUT);
-  }
-}
 
 void runTests() {
   testLoadDataHigh();
@@ -91,21 +31,13 @@ void runTests() {
   testOutputEnable();
 }
 
-void foo() {
-  Serial.println("Starting load from address test.");
-  setAddressOutput();
-  setAddress(0x01);
-  digitalWrite(CTRL_BUS_SELECT, LOW);
-  digitalWrite(ADDR_1, LOW);
-}
-
 void testLoadDataHigh() {
   Serial.println("Starting load high test.");
   int data = 1;
   while (data <= 0x8) {
-    loadData(CTRL_LOAD_TH, data);
+    loadDataHigh(data);
     delayMicroseconds(1);
-    verifyData(CTRL_OE_TH, data);
+    verifyDataHigh(data);
     data = data << 1;
   }
 }
@@ -114,9 +46,9 @@ void testLoadDataLow() {
   Serial.println("Starting load low test.");
   int data = 1;
   while (data <= 0x8) {
-    loadData(CTRL_LOAD_TL, data);
+    loadDataLow(data);
     delayMicroseconds(1);
-    verifyData(CTRL_OE_TL, data);
+    verifyDataLow(data);
     data = data << 1;
   }
 }
@@ -143,14 +75,16 @@ void testOutputEnable() {
   int data_low = address & 0xf;
   int data_high = (address & 0xf0) >> 4;
   verifyAddress(address);
-  verifyData(CTRL_OE_TL, data_low);
-  verifyData(CTRL_OE_TH, data_high);
+  verifyDataLow(data_low);
+  verifyDataHigh(data_high);
 
-  digitalWrite(CTRL_OE_T, HIGH);
-  digitalWrite(CTRL_OE_TL, HIGH);
-  digitalWrite(CTRL_OE_TH, HIGH);
+  utils.resetControlPins();
+  loadAddress(0x00);
+  utils.resetControlPins();
 
-  int data = readData();
+  utils.setDataInput();
+  delay(1);
+  int data = utils.readData();
   if (data != 0xf) {
     char buf[80];
     sprintf(buf, "[FAIL] Expected 0xf, received %x", data);
@@ -159,8 +93,10 @@ void testOutputEnable() {
     Serial.println("[PASS] All data lines are high.");
   }
 
-  address = readData();
-  if (address != 0xf) {
+  utils.setAddressInput();
+  delay(1);
+  address = utils.readAddress();
+  if (address != 0xff) {
     char buf[80];
     sprintf(buf, "[FAIL] Expected 0xff, received %x", address);
     Serial.println(buf);
@@ -169,20 +105,47 @@ void testOutputEnable() {
   }
 }
 
-void loadData(int loadPin, int data) {
-  setDataOutput();
-  setData(data);
-  digitalWrite(CTRL_BUS_SELECT, HIGH);
-  digitalWrite(loadPin, LOW);
-  pulseClock();
-  digitalWrite(loadPin, HIGH);
+void loadDataHigh(int data)
+{
+  utils.setDataOutput();
+  utils.assertData(data);
+  utils.assertTransferInputFromDataBus();
+  utils.assertTransferHighLoad();
+  utils.pulseClock(1);
+  utils.resetControlPins();
 }
 
-void verifyData(int outputEnablePin, int expected) {
-  setDataInput();
-  digitalWrite(outputEnablePin, LOW);
-  int data = readData();
-  digitalWrite(outputEnablePin, HIGH);
+void loadDataLow(int data)
+{
+  utils.setDataOutput();
+  utils.assertData(data);
+  utils.assertTransferInputFromDataBus();
+  utils.assertTransferLowLoad();
+  utils.pulseClock(1);
+  utils.resetControlPins();
+}
+
+void verifyDataHigh(int expected)
+{
+  utils.setDataInput();
+  utils.assertTransferHighOutputEnable();
+  int data = utils.readData();
+  utils.resetControlPins();
+  if (data != expected) {
+    char buf[80];
+    sprintf(buf, "[FAIL] Expected %x, received %x", expected, data);
+    Serial.println(buf);
+  } else {
+    Serial.println("[PASS] Load data passed.");
+  }
+}
+
+void verifyDataLow(int expected)
+{
+  utils.setDataInput();
+  utils.assertTransferLowOutputEnable();
+  int data = utils.readData();
+  utils.resetControlPins();
   if (data != expected) {
     char buf[80];
     sprintf(buf, "[FAIL] Expected %x, received %x", expected, data);
@@ -193,21 +156,20 @@ void verifyData(int outputEnablePin, int expected) {
 }
 
 void loadAddress(int data) {
-  setAddressOutput();
-  setAddress(data);
-  digitalWrite(CTRL_BUS_SELECT, LOW);
-  digitalWrite(CTRL_LOAD_TH, LOW);
-  digitalWrite(CTRL_LOAD_TL, LOW);
-  pulseClock();
-  digitalWrite(CTRL_LOAD_TH, HIGH);
-  digitalWrite(CTRL_LOAD_TL, HIGH);
+  utils.setAddressOutput();
+  utils.assertAddress(data);
+  utils.assertTransferInputFromAddressBus();
+  utils.assertTransferLowLoad();
+  utils.assertTransferHighLoad();
+  utils.pulseClock(1);
+  utils.resetControlPins();
 }
 
 void verifyAddress(int expected) {
-  setAddressInput();
-  digitalWrite(CTRL_OE_T, LOW);
-  int address = readAddress();
-  digitalWrite(CTRL_OE_T, HIGH);
+  utils.setAddressInput();
+  utils.assertTransferOutputEnable();
+  int address = utils.readAddress();
+  utils.resetControlPins();
   if (address != expected) {
     char buf[80];
     sprintf(buf, "[FAIL] Expected %0x, received %0x", expected, address);
@@ -215,46 +177,6 @@ void verifyAddress(int expected) {
   } else {
     Serial.println("[PASS] Load from address passed.");
   }
-}
-
-void pulseClock() {
-  digitalWrite(CTRL_CLK, LOW);
-  delayMicroseconds(1);
-  digitalWrite (CTRL_CLK, HIGH);
-  delayMicroseconds(1);
-  digitalWrite(CTRL_CLK, LOW);
-}
-
-void setAddress(int address) {
-  int values[8];
-  for (int i = 0; i < 8; i++) {
-    values[i] = (address >> i) & 1;
-    digitalWrite(address_pins[i], values[i]);
-  }
-}
-
-int readAddress() {
-  int address = 0;
-  for (int i = 7; i >= 0; i--) {
-    address = (address << 1) | digitalRead(address_pins[i]);
-  }
-  return address;
-}
-
-void setData(int data) {
-  int values[4];
-  for (int i = 0; i < 4; i++) {
-    values[i] = (data >> i) & 1;
-    digitalWrite(data_pins[i], values[i]);
-  }
-}
-
-int readData() {
-  int data = 0;
-  for (int i = 3; i >= 0; i--) {
-    data = (data << 1) | digitalRead(data_pins[i]);
-  }
-  return data;
 }
 
 void toBinary(int bits, int number, char *buf) {
